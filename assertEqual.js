4f0494a8ce75b4b49df0e6bdafcfbac040fb3eeb
MitchScho <scho.mitch@gmail.com>
2022-04-05 03:36:37 +0000

    return head of array with head function

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ ✅ Assertion Passed ✅ ✅: " + actual + " === " + expected);
+    console.log(`✅ ✅ Assertion Passed ✅ ✅: ${actual} === ${expected}`);
   } else {
-    console.log("💥💥Assertion Failed💥💥: " + actual + " !== " + expected);
+    console.log(`💥💥Assertion Failed💥💥: ${actual} !== ${expected}`);
   }
 };
 
