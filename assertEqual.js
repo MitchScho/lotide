41022e5c42dfe95cc36546bf9f063746cc1909f3
MitchScho <scho.mitch@gmail.com>
2022-04-05 02:22:30 +0000

    Assertion function that compares two values and prints out a coulourful successful or failure message

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ ✅ Assertion Passed ✅ ✅: " + actual + " === " + expected);
+  } else {
+    console.log("💥💥Assertion Failed💥💥: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual(15, 1);
