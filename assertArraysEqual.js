1b3fe2afc16ec5186465c103793a1db9f88d85db
MitchScho <scho.mitch@gmail.com>
2022-04-06 21:28:53 +0000

    Assertion for array comparison

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+//function receives two arrays
+const eqArrays = function (arrOne, arrTwo) {
+  //Compare the two arrays
+  //return false if arrays don't match
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  //Return true if arrays match
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  const result = eqArrays(actual, expected);
+  if (result === true) {
+    console.log(`✅ ✅ Assertion Passed ✅ ✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`💥💥Assertion Failed💥💥: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
