323431936d058606200ec7562b538806bbaf6a90
MitchScho <scho.mitch@gmail.com>
2022-04-07 05:43:54 +0000

    create letter position function

---
+++ letterPosition.js
@@ -0,0 +1,37 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+letterPositions("hello");
+const eqArrays = function(arrOne, arrTwo) {
+  //Compare the two arrays
+  //return false if arrays don't match
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  //Return true if arrays match
+  return true;
+};
+const assertArraysEqual = function (actual, expected) {
+  const result = eqArrays(actual, expected);
+  if (result === true) {
+    console.log(`✅ ✅ Assertion Passed ✅ ✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`💥💥Assertion Failed💥💥: ${actual} !== ${expected}`);
+  }
+};
+assertArraysEqual(letterPositions("hello").e, [1]);
