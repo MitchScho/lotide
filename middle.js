b5eea7285ea4b143e1df0322c48d92432b223c44
MitchScho <scho.mitch@gmail.com>
2022-04-11 20:27:49 +0000

    implement require and module exports to file while refactoring

--- middle.js
+++ middle.js
@@ -1,29 +1,4 @@
-//function receives two arrays
-const eqArrays = function (arrOne, arrTwo) {
-  //Compare the two arrays
-  //return false if arrays don't match
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrOne.length; i++) {
-    if (arrOne[i] !== arrTwo[i]) {
-      return false;
-    }
-  }
-  //Return true if arrays match
-  return true;
-};
 
-const assertArraysEqual = function (actual, expected) {
-  const result = eqArrays(actual, expected);
-  if (result === true) {
-    console.log(`✅ ✅ Assertion Passed ✅ ✅: ${actual} === ${expected}`);
-  } else {
-    console.log(`💥💥Assertion Failed💥💥: ${actual} !== ${expected}`);
-  }
-};
-
-//assertArraysEqual([1, 2, 3], [1, 2, 3]);
 
 const middle = function (array) {
   
@@ -48,16 +23,8 @@ const middle = function (array) {
   
 };
 
-
+module.exports = middle;
 //Should return an array
 //Should return only the middle element or elements
 //If the array is an even number the two elements in the centre should be returned
 //If the array is of an odd number the single middle element should be returned
-// middle([1]);
-// middle([1, 2]);
-// middle([1, 2, 3, 4, 5]);
-// middle([1, 2, 8, 4, 5, 12]);
-assertArraysEqual(middle([1]), [ ]);
-assertArraysEqual(middle([1, 2]), [ ]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [ 3]);
-assertArraysEqual(middle([1, 2, 8, 4, 5, 12]), [ 8, 4]);
